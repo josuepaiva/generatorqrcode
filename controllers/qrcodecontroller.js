d28f1b7704e0c35ee4c46bf0670c6d75c2db9a15
Josue <josuepaiva@lavid.ufpb.br>
2018-05-23 11:08:40 -0300

    links adicionados nas images qrcode

--- controllers/qrcodecontroller.js
+++ controllers/qrcodecontroller.js
@@ -11,27 +11,31 @@ exports.gerarQRCode = (req, res, next) => {
   
   const number = Math.floor(Math.random() * global.pessoas.length);
   const url = 'https://enigmatic-hamlet-84089.herokuapp.com/qrcode/exibe/';
+  const qrcodes = [];
 
   gravar_versao1(number, url)
-    .then(function(conteudo){
+    .then(function(versao1){
+      qrcodes.push(versao1);
     return gravar_versao40(number, url);
-  }).then(function(conteudo){
+  }).then(function(versao40){
+      qrcodes.push(versao40);
     res.render('generatorqrcode', { idqrcode : number });
+    // res.status(200).json(qrcodes);
   }); 
 }
 
 exports.exibe = (req, res, next ) => {
-
+  
   if (req.params.id !== null) {
-    console.log(global.pessoas[req.params.id]);
-    res.status(200).json(global.pessoas[req.params.id]);
-    // res.render('usuario', { usuario: global.pessoas[req.params.id]} );
+    // res.status(200).json(global.pessoas[req.params.id]);
+    res.render('usuario', { usuario: global.pessoas[req.params.id]} );
   }else{
-    res.status(500).json({ message: 'Id invalido'});
+    res.render('error', { message: 'Id invalido'});
+    // res.status(500).json({ message: 'Id invalido'});
   }
 }
 
-exports.index = (req, res, next) => {
+exports.index = (req, res, next) => { 
   res.render('index', { title : 'Gerador qrcode'} );
 }
 
@@ -42,7 +46,7 @@ function gravar_versao1(number, url){
   var qr_versao1 = qr.imageSync(url + number, { type: 'png' });
 
   fs.writeFile('public/images/qr_versao1.png', qr_versao1, function(err){
-    deferred.resolve(true);
+    deferred.resolve('public/images/qr_versao1.png');
   });
 
   return deferred.promise;
@@ -55,7 +59,7 @@ function gravar_versao40(number){
   var qr_versao1 = qr.imageSync(JSON.stringify(global.pessoas[number]), { type: 'png', size: 40 });
 
   fs.writeFile('public/images/qr_versao40.png', qr_versao1, function(err){
-    deferred.resolve(true);
+    deferred.resolve('public/images/qr_versao40.png');
   });
 
   return deferred.promise;
